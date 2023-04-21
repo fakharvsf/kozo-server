de3fe60eff746e1d58f72ff84affa71ef816c10b
theVenom <afrozopsy.dev@gmail.com>
2023-04-22 04:06:14 +0500

    Fix server build issues.

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ func main() {
 		panic("Please run as 'go run . rest dev' or 'go run . socket dev")
 	}
 
-	var env string
+	var env string = os.Getenv("dir")
 
 	if(args[1] == "dev"){
-		env = ".env.dev"
+		env = env + ".env.dev"
 	} else if (args[1] == "prod"){
-		env = ".env.prod"
+		env = env + ".env.prod"
 	}
 
 	if args[0] == "rest" {
